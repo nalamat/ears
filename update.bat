ab2a22ad3211211f975a50c1a648bd940d00fa97
Nima Alamatsaz <nima.alamatsaz@gmail.com>
2018-07-06 16:55:44 -0400

    Pull master

--- update.bat
+++ update.bat
@@ -1,6 +1,8 @@
 @echo off
 
-(goto 2>nul & git fetch -t && ^
+(goto 2>nul & ^
+git fetch -t && ^
 git checkout master -f && ^
+git pull && ^
 echo: && echo Update successful ...) || (
 echo: && echo Update failed ... && pause >nul)
