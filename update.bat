273982e9cf2a3438c42468133958e4e9fa59e396
Nima Alamatsaz <nima.alamatsaz@gmail.com>
2018-07-06 16:15:03 -0400

    Fetch with tags

--- update.bat
+++ update.bat
@@ -1,6 +1,6 @@
 @echo off
 
-(goto 2>nul & git fetch && ^
+(goto 2>nul & git fetch -t && ^
 git checkout RC -f && ^
 echo: && echo Update successful ...) || ^
 (echo: && echo Update failed ... && pause >nul)
