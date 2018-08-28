7040cce730367155d9bf5d249e5baa38289a774a
Nima Alamatsaz <nialamat@gmail.com>
2018-08-28 13:20:36 -0400

    Brief copyright notice

--- setup.py
+++ setup.py
@@ -4,16 +4,7 @@
 This file is part of the EARS project: https://github.com/nalamat/ears
 Copyright (C) 2017-2018 Nima Alamatsaz <nima.alamatsaz@njit.edu>
 Copyright (C) 2017-2018 Antje Ihlefeld <antje.ihlefeld@njit.edu>
-
-This program is free software: you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free Software
-Foundation, either version 3 of the License, or (at your option) any later
-version.
-This program is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-PARTICULAR PURPOSE. See the GNU General Public License for more details.
-You should have received a copy of the GNU General Public License along with
-this program. If not, see <http://www.gnu.org/licenses/>.
+Distrubeted under GNU GPLv3. See LICENSE.txt for more info.
 '''
 
 import os
